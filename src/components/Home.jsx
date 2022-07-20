76fd973997522c9dae9abc0180510f17d5b56668
kylieogg <kylieogg1@gmail.com>
2022-07-20 09:49:00 -0400

    completed home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,23 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-gray-900'>
+    <div name='home' className='w-full h-screen bg-gray-900'>
 
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Kylie Ogg</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Front End Web Developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I am focused on building and designing responsive front-end web applications.</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
+                <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3' />
+                </span>
+                </button>
+            </div>
+
+        </div>
     </div>
   )
 }
