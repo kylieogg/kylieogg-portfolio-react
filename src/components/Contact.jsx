2365f1004ea181d13013530c936ee8827fa277ab
kylieogg <kylieogg1@gmail.com>
2022-07-21 11:54:16 -0400

    linked contact to getform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-gray-900 flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/8355105a-935c-45e1-bd96-721c23d3bbf5" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or email me at kylieogg1@gmail.com</p>
