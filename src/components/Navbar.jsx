fc2194fe2f086f080337819aabe142e0a947e4c6
kylieogg <kylieogg1@gmail.com>
2022-07-21 12:13:25 -0400

    added onclick event for mobile menu in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,11 +51,21 @@ const Navbar = () => {
 
             {/* Mobile Menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-gray-900 flex flex-col justify-center items-center'} >
-               <li className='py-6 text-4xl'>Home</li>
-               <li className='py-6 text-4xl'>About</li>
-               <li className='py-6 text-4xl'>Skills</li>
-               <li className='py-6 text-4xl'>Projects</li>
-               <li className='py-6 text-4xl'>Contact</li> 
+               <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>
+          Home
+        </Link> </li>
+               <li className='py-6 text-4xl'><Link onClick={handleClick} to="about" smooth={true} duration={500}>
+          About
+        </Link> </li>
+               <li className='py-6 text-4xl'><Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+          Skills
+        </Link> </li>
+               <li className='py-6 text-4xl'><Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+          Projects
+        </Link> </li>
+               <li className='py-6 text-4xl'><Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+          Contact
+        </Link> </li> 
             </ul>
 
 
