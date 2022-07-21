44b7f70f5b2d780ca97ddfb2c4765fa6287f9245
kylieogg <kylieogg1@gmail.com>
2022-07-21 12:05:10 -0400

    linked nav sections via react smooth scroll install

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import Logo from '../assets/logo.png';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -16,11 +17,31 @@ const Navbar = () => {
        
        {/* Menu */}
            <ul className='hidden md:flex'>
-               <li>Home</li>
-               <li>About</li>
-               <li>Skills</li>
-               <li>Projects</li>
-               <li>Contact</li>
+               <li>
+               <Link to="home" smooth={true} duration={500}>
+          Home
+        </Link> 
+               </li>
+               <li>
+               <Link to="about" smooth={true} duration={500}>
+          About
+        </Link> 
+               </li>
+               <li>
+               <Link to="skills" smooth={true} duration={500}>
+          Skills
+        </Link> 
+               </li>
+               <li>
+               <Link to="projects" smooth={true} duration={500}>
+          Projects
+        </Link> 
+               </li>
+               <li>
+               <Link to="contact" smooth={true} duration={500}>
+          Contact
+        </Link> 
+               </li>
            </ul> 
 
         {/* Hamburger */}
